824aa8ffa98a067adc65ac1f3b14af228b04bfbb
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2019-07-27 22:10:34 +0200

    Add files via upload

---
+++ 5 rank/Basic Nico variation/Basic Nico variation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Nico_variation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = Nico("crazy", "secretinformation");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+        public static string Nico(string key, string message)
+        {
+            string[] s= new string[key.Length];
+            Dictionary<char, string> d = new Dictionary<char, string>();
+            for (int i = 0, j = 0; i < message.Length; i++, j++)
+            {
+                if (j == key.Length)
+                    j = 0;
+
+                s[j] += message[i];
+            }
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[0].Length != s[i].Length)
+                    s[i] += " ";
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                d.Add(key[i], s[i]);
+            }
+            //var items = from pair in d
+            //            orderby pair.Key ascending
+            //            select pair;
+            string result = "";
+            foreach (KeyValuePair<char, string> sorted in d.OrderBy(i => i.Key))
+            {
+                result += sorted.Value;
+            }
+            return result;
+            //2 3 1 5 4
+            //-------- -
+            //s e c r e
+            //t i n f o
+            //r m a t i
+            //o n
+            //After using the key:
+
+            //1 2 3 4 5
+            //-------- -
+            //c s e e r
+            //n t i o f
+            //a r m i t
+            //  o n
+
+        }
+    }
+}
---
+++ 5 rank/Car Park Escape/Car Park Escape/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Car_Park_Escape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] carpark = new int[,] { { 1, 0, 0, 0, 0 },
+                                          { 0, 0, 0, 0, 1 },
+                                          { 1, 0, 2, 0, 0 },
+                                          { 0, 0, 0, 0, 0 } };
+            string[] result = escape(carpark);
+            foreach(string element in result )
+                Console.WriteLine(element);
+            Console.ReadLine();
+        }
+        static public string[] escape(int[,] carpark)
+        {
+            string result = "";
+            int start = 0;
+            int staircases = -1;
+            int down = 0;
+            //for (int i = 0; i < carpark.GetLength(0); i++)
+            //{
+            //    if (carpark.GetLength(0) - 1 == i)
+            //        staircases = carpark.GetLength(1) - 1;
+            //    for (int j = 0; j < carpark.GetLength(1); j++)
+            //    {
+            //        if (carpark[i, j] == 1)
+            //            staircases = j;
+            //        else if(carpark[i, j] == 2)
+            //        {
+            //            start = j;
+            //        }                                  
+            //    }
+            //    if(start - staircases !=0)
+            //        result += (staircases != -1 && start - staircases > 0) ? "L" + (start - staircases) + " " :
+            //            "R" + Math.Abs(start - staircases) + " ";
+            //    for (int k = i; k < carpark.GetLength(0); k++)
+            //    {
+            //        if (carpark[k, staircases] == 1)
+            //            down++;
+            //        else
+            //        {
+            //            carpark[k, staircases] = 2;
+            //            break;
+            //        }
+            //    }
+            //    if (carpark.GetLength(0) - 1 != i)
+            //        result += "D" + down+ " ";
+            //    i += (down > 0)? down - 1 : 0;
+            //    down = 0;
+            //    staircases = -1;
+            //}
+            for (int i = 0; i < carpark.GetLength(0); i++)
+            {
+                for (int j = 0; j < carpark.GetLength(1); j++)
+                {
+                    if (carpark[i, j] == 2)
+                    {
+                        down = i;
+                        start = j;
+                    }
+                }
+            }
+
+            for (int i = down; i < carpark.GetLength(0);)
+            {
+                down = 0;
+                for (int j = 0; j < carpark.GetLength(1); j++)
+                {
+                    if (carpark[i, j] == 1)
+                        staircases = j;
+                }
+
+                if (staircases == -1)
+                {
+                    staircases = carpark.GetLength(1) - 1;
+                    if (start != staircases)
+                    {
+                        result += ((start - staircases) > 0) ? "L" : "R";
+                        result += Math.Abs(start - staircases) + " ";
+                    }
+                    break;
+                }
+                else
+                { 
+                    result += ((start - staircases) > 0) ? "L" : "R";
+                    result += Math.Abs(start - staircases) + " ";
+                    start = staircases;
+                }
+                for (int j = i; j < carpark.GetLength(0); j++)
+                {
+                    if (carpark[j, start] == 1)
+                        down++;
+                    else
+                        break;
+                }
+                result += (down > 0) ? "D" + down + " ": "";
+                i += down;
+                down = 0;
+                staircases = -1;
+            }
+            return result.Split( new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+    }
+}
