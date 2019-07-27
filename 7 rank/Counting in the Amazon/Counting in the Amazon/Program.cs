985d3549acf16a4d9f54eb3dea749f69d823933d
Robert <42147723+RobertKrolikowski@users.noreply.github.com>
2019-07-27 22:20:51 +0200

    Add files via upload

---
+++ 7 rank/Counting in the Amazon/Counting in the Amazon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Counting_in_the_Amazon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountArara(3));
+            Console.ReadKey();
+        }
+
+        public static string CountArara(int n)
+        {
+            //adak anane
+            string result = "";
+            for(int i = 1; i < n; i+=2)
+            {
+                result += (i + 2 < n || n % 2 != 0) ? "adak " : "adak";
+            }
+
+            if (n % 2 != 0)
+                result += "anane";
+            
+
+            return result;
+        }
+    }
+}
---
+++ 7 rank/Determine if the poker hand is flush/Determine if the poker hand is flush/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Determine_if_the_poker_hand_is_flush
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static bool CheckIfFlush(string[] cards)
+        {
+            int counter = 0;
+            for (int i = 0; i < cards.Length - 1; i++)
+            {
+                if (cards[i][cards[i].Length - 1] == cards[i + 1][cards[i+1].Length - 1])
+                    counter++;
+            }
+            if (counter == 4)
+                return true;
+            else
+                return false;
+
+
+        }
+    }
+}
